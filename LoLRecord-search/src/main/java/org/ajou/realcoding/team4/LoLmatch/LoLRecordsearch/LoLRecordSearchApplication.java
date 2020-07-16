58ec2adef864bc53c6849a9531cec0821c69ca3a
thdl <thdl9893@ajou.ac.kr>
2020-07-16 22:31:20 +0900

    Make File for project

---
+++ LoLRecord-search/src/main/java/org/ajou/realcoding/team4/LoLmatch/LoLRecordsearch/LoLRecordSearchApplication.java
@@ -0,0 +1,13 @@
+package org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoLRecordSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoLRecordSearchApplication.class, args);
+	}
+
+}
