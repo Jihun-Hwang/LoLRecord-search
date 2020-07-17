0244e94cbee413258b7bcda17bd51f202b9b54fb
Hyung-Taik Choi <htc.refactor@gmail.com>
2020-07-17 13:21:30 +0900

    Added example response body of LEAGUE API to League class.

--- LoLRecord-search/src/main/java/org/ajou/realcoding/team4/LoLmatch/LoLRecordsearch/domain/League.java
+++ LoLRecord-search/src/main/java/org/ajou/realcoding/team4/LoLmatch/LoLRecordsearch/domain/League.java
@@ -1,4 +1,21 @@
 package org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.domain;
 
 public class League {
+    /*
+    {
+        "leagueId": "2d916f43-ef5a-4625-8533-f943f7f99c81",
+            "queueType": "RANKED_SOLO_5x5",
+            "tier": "SILVER",
+            "rank": "III",
+            "summonerId": "cTJRw-gsfTz7Rd5Yy-MFegnTIyeM2rCIPeWYPgB4hPrrIsk",
+            "summonerName": "BLACKGREYWHITE",
+            "leaguePoints": 8,
+            "wins": 33,
+            "losses": 38,
+            "veteran": false,
+            "inactive": false,
+            "freshBlood": false,
+            "hotStreak": false
+    }
+    */
 }
