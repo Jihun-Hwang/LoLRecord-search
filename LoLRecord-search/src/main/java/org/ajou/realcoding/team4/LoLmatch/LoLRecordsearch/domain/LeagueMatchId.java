81d3286804be99c7e39a1692222cb8cab3711e34
yejoonko <yejoon08@ajou.ac.kr>
2020-07-18 03:03:01 +0900

    modify to save only game ID

--- LoLRecord-search/src/main/java/org/ajou/realcoding/team4/LoLmatch/LoLRecordsearch/domain/LeagueMatchId.java
+++ LoLRecord-search/src/main/java/org/ajou/realcoding/team4/LoLmatch/LoLRecordsearch/domain/LeagueMatchId.java
@@ -3,10 +3,30 @@ package org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.domain;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class LeagueMatchId {
-    @Id
-    private long gameId;
-    private int champion;
+   // @Id
+   // private int startIndex;
+   // private int endIndex;
+   // private int totalGames;
+    private List<Matches> matches;
+
+    @Data
+    public static class Matches{
+     //   private String platformId;
+        private long gameId;
+      //  private int champion;
+      //  private int queue;
+      //  private int season;
+      //  private long timestamp;
+      //  private String role;
+       // private String lane;
+
+    }
+
+
 }
 
