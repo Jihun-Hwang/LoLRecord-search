e609bd26370dcccb9ca9824747256976e42d0ef0
Hyung-Taik Choi <htc.refactor@gmail.com>
2020-07-17 15:44:08 +0900

    Added saveLeagueEntry method to LeagueEntryRepository class.

--- LoLRecord-search/src/main/java/org/ajou/realcoding/team4/LoLmatch/LoLRecordsearch/repository/LeagueEntryRepository.java
+++ LoLRecord-search/src/main/java/org/ajou/realcoding/team4/LoLmatch/LoLRecordsearch/repository/LeagueEntryRepository.java
@@ -1,4 +1,19 @@
 package org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.repository;
 
+import lombok.extern.slf4j.Slf4j;
+import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.domain.LeagueEntry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Slf4j
 public class LeagueEntryRepository {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    private void saveLeagueEntry(LeagueEntry leagueEntry) {
+        LeagueEntry savedLeagueEntry = mongoTemplate.save(leagueEntry[0]);
+        log.info("Saved: {}", savedLeagueEntry);
+    }
 }
