9062e393ca87c35b7af4cb77c4ef1500da14764d
yejoonko <yejoon08@ajou.ac.kr>
2020-07-18 03:28:48 +0900

    modify db update 1.5s when EncryptedAccountId isn't null.

--- LoLRecord-search/src/main/java/org/ajou/realcoding/team4/LoLmatch/LoLRecordsearch/service/LeagueMatchIdService.java
+++ LoLRecord-search/src/main/java/org/ajou/realcoding/team4/LoLmatch/LoLRecordsearch/service/LeagueMatchIdService.java
@@ -7,10 +7,18 @@ import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.domain.LeagueMatchId;
 import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.repository.LeagueEntryRepository;
 import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.repository.LeagueMatchIdRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 
 @Service
+@EnableScheduling
 public class LeagueMatchIdService {
     @Autowired
     private LeagueMatchIdApiClient leagueMatchIdApiClient;
@@ -18,9 +26,24 @@ public class LeagueMatchIdService {
     @Autowired
     private LeagueMatchIdRepository leagueMatchIdRepository;
 
-    public LeagueMatchId[] getFiveLeagueMatchId(String encryptedAccountId, String apiKey){
-        LeagueMatchId[] leagueMatchIds = leagueMatchIdApiClient.getMatchIdRecentFive(encryptedAccountId,apiKey);
-        leagueMatchIdRepository.saveFiveLeagueMatchID(leagueMatchIds);
-        return leagueMatchIds;
+    private Queue<String> savedEncryptedAccountId = new LinkedList<>();
+    String apiKey;
+
+    public LeagueMatchId getFiveLeagueMatchId (String encryptedAccountId, String apiKey) {
+        LeagueMatchId leagueMatchId = leagueMatchIdApiClient.getMatchIdRecentFive(encryptedAccountId,apiKey);
+        savedEncryptedAccountId.add(encryptedAccountId);
+        //leagueMatchIdRepository.saveFiveLeagueMatchID(leagueMatchId);
+        this.apiKey = apiKey;
+        return leagueMatchId;
+    }
+
+    @Scheduled(fixedDelay = 1500L)
+    public void getCurrentLoLFiveGameIDsEveryTwoSeconds(){
+        String targetEncryptedAccountId = savedEncryptedAccountId.poll();
+        if(targetEncryptedAccountId != null){
+            savedEncryptedAccountId.add(targetEncryptedAccountId);
+            LeagueMatchId leagueMatchId = leagueMatchIdApiClient.getMatchIdRecentFive(targetEncryptedAccountId,this.apiKey);
+            leagueMatchIdRepository.saveFiveLeagueMatchID(leagueMatchId);
+        }
     }
 }
