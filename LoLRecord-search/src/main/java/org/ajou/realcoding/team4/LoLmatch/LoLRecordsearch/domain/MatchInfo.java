9d60109a54904d49893429a966eccfe4275786bf
Jihun-Hwang <goharrm@ajou.ac.kr>
2020-07-19 18:04:49 +0900

    add new domain class named 'MatchInfo'

---
+++ LoLRecord-search/src/main/java/org/ajou/realcoding/team4/LoLmatch/LoLRecordsearch/domain/MatchInfo.java
@@ -0,0 +1,37 @@
+package org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class MatchInfo {  // Match information
+    private int gameId;
+    private List<Teams> teams;
+    private List<Participants> participants;
+
+    @Data
+    public static class Teams{
+        private int teamId;
+        boolean win;
+    }
+
+    @Data
+    public static class Participants{ // Information about the requested summoner
+        private int participantId;
+        private int teamId;
+        private int championId;
+        private Stats stats;
+
+        @Data
+        public static class Stats{
+            private int participantId;
+            private boolean win;
+            private int kills;
+            private int deaths;
+            private int assists;
+        }
+
+    }
+
+}
