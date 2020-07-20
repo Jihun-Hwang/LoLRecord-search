de42e03520dcc0aba860577bbf5e0bad212fc370
Jihun-Hwang <goharrm@ajou.ac.kr>
2020-07-20 15:25:40 +0900

    LeagueApiService class renamed to LeagueInfoService

--- LoLRecord-search/src/main/java/org/ajou/realcoding/team4/LoLmatch/LoLRecordsearch/service/LeagueInfoService.java
+++ LoLRecord-search/src/main/java/org/ajou/realcoding/team4/LoLmatch/LoLRecordsearch/service/LeagueInfoService.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class LeagueApiService {
+public class LeagueInfoService {
     @Autowired
     private LeagueApiClient leagueApiClient;
 
