9b2f36ab343ebf21f6a89bdc94b2ddd08e38b4cd
yejoonko <yejoon08@ajou.ac.kr>
2020-07-20 21:49:34 +0900

    Modified return value and code because we want to return only one Summoner MatchInfo.

--- LoLRecord-search/src/main/java/org/ajou/realcoding/team4/LoLmatch/LoLRecordsearch/service/MatchInfoService.java
+++ LoLRecord-search/src/main/java/org/ajou/realcoding/team4/LoLmatch/LoLRecordsearch/service/MatchInfoService.java
@@ -3,6 +3,7 @@ package org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.service;
 import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.api.MatchInfoApiClient;
 import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.domain.MatchId;
 import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.domain.MatchInfo;
+import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.domain.SummonerMatchInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,14 +15,32 @@ public class MatchInfoService {
     @Autowired
     private MatchInfoApiClient matchInfoApiClient;
 
-    public List<MatchInfo> getMatchInfoList(MatchId currentFiveLeagueMatchId, String apiKey) {
-        List<MatchInfo> matchInfoList = new LinkedList<>();
+    public List<SummonerMatchInfo> getMatchInfoList(String name,MatchId currentFiveLeagueMatchId, String apiKey) {
+        //List<MatchInfo> matchInfoList = new LinkedList<>();
+        List<SummonerMatchInfo> summonerMatchInfoList = new LinkedList<>();
         for(int i = 0; i < 5; i++){
-            matchInfoList.add(
-                    matchInfoApiClient.getMatchInfoApi(currentFiveLeagueMatchId.getMatches().get(i).getGameId(), apiKey)
-            );
+            MatchInfo tempMatchInfo = matchInfoApiClient.getMatchInfoApi(currentFiveLeagueMatchId.getMatches().get(i).getGameId(), apiKey);
+            SummonerMatchInfo tempSummonerMatchInfo = new SummonerMatchInfo();
+            tempSummonerMatchInfo.setGameId(tempMatchInfo.getGameId());
+
+            for(int j=0;j<10;j++){
+                String tempName = tempMatchInfo.getParticipantIdentities().get(j).getPlayer().getSummonerName();
+                if(tempName.equals(name)){
+                    tempSummonerMatchInfo.setName(name);
+                    tempSummonerMatchInfo.setParticipantId(tempMatchInfo.getParticipantIdentities().get(j).getParticipantId());
+                    tempSummonerMatchInfo.setTeamId(tempMatchInfo.getParticipants().get(j).getTeamId());
+                    tempSummonerMatchInfo.setChampionId(tempMatchInfo.getParticipants().get(j).getChampionId());
+                    tempSummonerMatchInfo.setParticipantId(tempMatchInfo.getParticipants().get(j).getParticipantId());
+                    tempSummonerMatchInfo.setAssists(tempMatchInfo.getParticipants().get(j).getStats().getAssists());
+                    tempSummonerMatchInfo.setDeaths(tempMatchInfo.getParticipants().get(j).getStats().getDeaths());
+                    tempSummonerMatchInfo.setKills(tempMatchInfo.getParticipants().get(j).getStats().getKills());
+                    tempSummonerMatchInfo.setWin(tempMatchInfo.getParticipants().get(j).getStats().isWin());
+                    break;
+                }
+            }
+            summonerMatchInfoList.add(tempSummonerMatchInfo);
         }
 
-        return matchInfoList;
+        return summonerMatchInfoList;
     }
 }
