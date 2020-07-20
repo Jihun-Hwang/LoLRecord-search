c31faa99b9c552c54e997fec7e8c70dfbe38e555
Jihun-Hwang <goharrm@ajou.ac.kr>
2020-07-20 15:58:26 +0900

    change variable name, method name, and erase comments

--- LoLRecord-search/src/main/java/org/ajou/realcoding/team4/LoLmatch/LoLRecordsearch/controller/SearchController.java
+++ LoLRecord-search/src/main/java/org/ajou/realcoding/team4/LoLmatch/LoLRecordsearch/controller/SearchController.java
@@ -1,14 +1,13 @@
 package org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.controller;
 
 import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.domain.LeagueEntry;
-import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.domain.LeagueMatchId;
+import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.domain.MatchId;
 import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.domain.MatchInfo;
 import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.domain.Summoner;
-import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.repository.LeagueMatchIdRepository;
-import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.service.LeagueApiService;
-import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.service.LeagueMatchIdService;
+import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.service.LeagueInfoService;
+import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.service.MatchIdService;
 import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.service.MatchInfoService;
-import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.service.SummonerApiService;
+import org.ajou.realcoding.team4.LoLmatch.LoLRecordsearch.service.SummonerInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,40 +17,30 @@ import java.util.List;
 
 @RestController
 public class SearchController {
-
     @Autowired
-    private SummonerApiService summonerApiService;
+    private SummonerInfoService summonerInfoService;
     @Autowired
-    private LeagueApiService leagueApiService;
+    private LeagueInfoService leagueInfoService;
     @Autowired
-    private LeagueMatchIdService leagueMatchIdService;
+    private MatchIdService MatchIdService;
     @Autowired
     private MatchInfoService matchInfoService;
 
-
-//    @Autowired
-//    private LeagueMatchIdService leagueMatchIdService;
-
     @GetMapping("/summonerApi-service/summoners")
-    public List<MatchInfo> getSummoner(@RequestParam String SummonerId, @RequestParam String apiKey) {
-        Summoner currentSummoner = summonerApiService.getSummoner(SummonerId, apiKey);  // step2
-        //LeagueEntry currentLeagueEntry = leagueApiService.getLeagueEntry(currentSummoner.getId(), apiKey); // step3
-        LeagueMatchId currentFiveLeagueMatchId = leagueMatchIdService.getFiveLeagueMatchId(currentSummoner.getAccountId(), apiKey); // step4
-        List<MatchInfo> matchInfoList = matchInfoService.getMatchInfoList(currentFiveLeagueMatchId, apiKey); // step5
+    public List<MatchInfo> getSummonerMatchInfo(@RequestParam String summonerId, @RequestParam String apiKey) {
+        // step2
+        Summoner currentSummonerInfo = summonerInfoService.getSummoner(summonerId, apiKey);
+
+        // step3
+        LeagueEntry currentLeagueEntry = leagueInfoService.getLeagueEntry(currentSummonerInfo.getId(), apiKey);
+
+        // step4
+        MatchId currentFiveMatchId = MatchIdService.getFiveMatchId(currentSummonerInfo.getAccountId(), apiKey);
+
+        // step5
+        List<MatchInfo> matchInfoList = matchInfoService.getMatchInfoList(currentFiveMatchId, apiKey);
 
         return matchInfoList;
     }
 
-/*
-    @GetMapping("/leagueApi-service/leagues")
-    public LeagueEntry getLeagueEntry(@RequestParam String encryptedSummonerId, @RequestParam String apiKey) {
-        return leagueApiService.getLeagueEntry(encryptedSummonerId, apiKey);
-    }
-/*
-    @GetMapping("/leagueMatch-service/matchIDs")
-    public LeagueMatchId getLFiveMatchId(@RequestParam String encryptedAccountId, @RequestParam String apiKey) {
-        return leagueMatchIdService.getFiveLeagueMatchId(encryptedAccountId, apiKey);
-    }
-
- */
 }
