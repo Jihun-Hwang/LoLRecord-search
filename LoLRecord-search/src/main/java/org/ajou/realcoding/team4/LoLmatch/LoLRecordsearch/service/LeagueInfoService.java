4e0d75f4aedd1486f5397152d807416d4a9a450b
Jihun-Hwang <goharrm@ajou.ac.kr>
2020-07-20 16:09:42 +0900

    erase an unneccessary field

--- LoLRecord-search/src/main/java/org/ajou/realcoding/team4/LoLmatch/LoLRecordsearch/service/LeagueInfoService.java
+++ LoLRecord-search/src/main/java/org/ajou/realcoding/team4/LoLmatch/LoLRecordsearch/service/LeagueInfoService.java
@@ -11,9 +11,6 @@ public class LeagueInfoService {
     @Autowired
     private LeagueApiClient leagueApiClient;
 
-    @Autowired
-    private LeagueEntryRepository leagueEntryRepository;
-
     public LeagueEntry getLeagueEntry(String encryptedSummonerId, String apiKey) {
         LeagueEntry[] leagueEntry = leagueApiClient.getLeagueEntry(encryptedSummonerId, apiKey);
         return leagueEntry[0];
